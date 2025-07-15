3f55106bde2bc879ac3f4ff8f7af46d257994d3d
Ezequiel Matías Unía <ezequiel.unia@mercadolibre.com>
2025-07-15 19:06:52 -0300

    add middlerware

--- main.go
+++ main.go
@@ -23,6 +23,21 @@ func main() {
 
 	r := gin.Default()
 
+	// Middleware para habilitar CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://ezequielunia.com.ar")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	r.POST("/send-email", func(c *gin.Context) {
 		log.Println("[INFO] Nueva petición POST /send-email")
 		var req EmailRequest
